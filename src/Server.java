6b5592cdddd3c9da665aeaa39017f7e29687ba86
erangeljr <hacker@eddierangel.com>
2013-05-09 21:04:07 -0700

    Delcaring Private members
    importing java namespace and adding private member variables for
    creating Server and Socket components.

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,13 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Server {
 
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	private Server connection;
+	private int counter = 1;
 }
