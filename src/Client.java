35f15f002f29824801d9f3608965c71a0d4b20db
erangeljr <hacker@eddierangel.com>
2013-05-12 11:55:52 -0700

    Importing Libraries
    Into Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,17 @@
+import java.io.EOFException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Client {
 	
