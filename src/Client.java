9c94f21379c75b8ff3c9ca519ea29542855ba2bb
erangeljr <hacker@eddierangel.com>
2013-05-12 11:59:37 -0700

    Adding Private Member Variables
    Plus adding components to Defautl Constructor

--- src/Client.java
+++ src/Client.java
@@ -13,10 +13,26 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Client {
+public class Client extends JFrame{
+	
+	private JTextField enterField;
+	private JTextArea displayArea;
+
+	//Output Stream to Client
+	private ObjectOutputStream output;
+	//Input Stream from Client
+	private ObjectInputStream input;
+	
+	private String message = "";
+	private String chatServer;
+	private Socket client;
 	
 	//Default Constructor
-	public Client(){
+	public Client(String host){
+		
+		super("Client Application");
+		
+		chatServer = host;
 		
 	}
 
