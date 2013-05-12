c4cc026bc3f8c8a5cd8404bc57f3f620e2dbd39c
erangeljr <hacker@eddierangel.com>
2013-05-11 20:29:51 -0700

    Added Default Constructor

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,9 @@
 
 public class Client {
+	
+	//Default Constructor
+	public Client(){
+		
+	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -3,11 +3,26 @@ import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class Server {
+public class Server implements Runnable{
 
+	//Output Stream to Client
 	private ObjectOutputStream output;
+	//Input Stream from Client
 	private ObjectInputStream input;
+	//ServerSocket
 	private ServerSocket server;
+	//Connection to Client
 	private Server connection;
 	private int counter = 1;
+	
+	//Default Constructor
+	public Server(){
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
